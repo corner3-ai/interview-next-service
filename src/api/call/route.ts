49dd7d858b820741e5e9cb475744365a6d79fc5e
Bry Nguyen <brynguyen1331@gmail.com>
2025-06-04 00:22:46 -0700

    add empty api/chat and

---
+++ src/api/call/route.ts
@@ -0,0 +1,1 @@
+// add the call API here
---
+++ src/app/call/page.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function Call() {
+  // maybe use the componnts fgrom livekit/coponents-react?
+  //
+  return <div>Call</div>
+}
