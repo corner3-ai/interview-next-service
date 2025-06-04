a97c31632b4bfabfcdac992015471e6d960a0051
Bry Nguyen <brynguyen1331@gmail.com>
2025-06-04 03:19:16 -0700

    Add auth api for connecting to livekit

---
+++ src/app/api/auth/route.ts
@@ -0,0 +1,59 @@
+import { NextRequest, NextResponse } from "next/server"
+import { AccessToken, VideoGrant } from "livekit-server-sdk"
+
+const apiKey = process.env.LIVEKIT_API_KEY
+const apiSecret = process.env.LIVEKIT_API_SECRET
+const livekitUrl = process.env.LIVEKIT_URL
+
+export type ConnectionParams = {
+  serverUrl: string
+  roomName: string
+  participantName: string
+  participantToken: string
+}
+
+export async function GET(request: NextRequest) {
+  try {
+    if (!apiKey || !apiSecret || !livekitUrl) {
+      throw new Error(
+        "LIVEKIT_API_KEY, LIVEKIT_API_SECRET, LIVEKIT_URL must be defined"
+      )
+    }
+
+    const { searchParams } = new URL(request.url)
+    const roomName = searchParams.get("roomName") || "interview-room"
+    const participantName =
+      searchParams.get("participantName") || `user-${Math.random() * 10_000}`
+
+    const at = new AccessToken(apiKey, apiSecret, {
+      identity: participantName,
+      ttl: "15m",
+    })
+
+    const videoGrant: VideoGrant = {
+      room: roomName,
+      roomJoin: true,
+      canPublish: true,
+      canPublishData: true,
+      canSubscribe: true,
+    }
+
+    at.addGrant(videoGrant)
+
+    const token = await at.toJwt()
+    const connectionParams: ConnectionParams = {
+      serverUrl: livekitUrl,
+      roomName,
+      participantName,
+      participantToken: token,
+    }
+
+    return NextResponse.json(connectionParams, { status: 200 })
+  } catch (error) {
+    console.error("Error generating LiveKit token:", error)
+    return NextResponse.json(
+      { error: "Failed to generate token" },
+      { status: 500 }
+    )
+  }
+}
